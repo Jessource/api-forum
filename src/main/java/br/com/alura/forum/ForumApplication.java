16cd509898b6df220662ba249542cf9b1afb0bd9
Jessica Souza <jsdl.9991@gmail.com>
2022-05-26 00:06:11 -0300

    implementação da paginação usando Pageable

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,10 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport// habilita o spring a pegar a requisição dos parametros da urls as informações de ordenação e paginação
 public class ForumApplication {
 
 	public static void main(String[] args) {
