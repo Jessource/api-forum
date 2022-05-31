89fc96119b0ea20e6a497d43c89846cb76a60c50
Jessica Souza <jsdl.9991@gmail.com>
2022-05-31 09:32:42 -0300

    Gerando o token

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,13 +1,16 @@
 package br.com.alura.forum.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AnyRequestMatcher;
 
@@ -20,6 +23,10 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter   {//es
 	
 	//configurações de autenticações/controle de login/acesso
 	@Override
+	@Bean
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(authenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
 	}
@@ -29,8 +36,10 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter   {//es
 			http.authorizeRequests()
 			.antMatchers(HttpMethod.GET, "/topicos").permitAll() //antMatchers libera os métodos que quero passar, no caso passei o get e mais alguma coisa
 			.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+			.antMatchers(HttpMethod.POST, "/auth").permitAll()
 			.anyRequest().authenticated()
-			.and().formLogin();//geralogin
+			.and().csrf().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//diz pra não criar a seção pq vai criar token
 		}
 	//confiurações statics/css/js/imagens
 	@Override
